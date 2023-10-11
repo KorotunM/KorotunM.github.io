ef61d0a64e532d99f236f1e9660b2521a72e9888
KorotunM <144013223+KorotunM@users.noreply.github.com>
2023-10-11 21:33:39 +0300

    Add files via upload

---
+++ KorotunTask5/function.js
@@ -0,0 +1,27 @@
+window.addEventListener('DOMContentLoaded', function (event)
+{
+    console.log("DOM fully loaded and parsed");
+    let b = document.getElementById("button");
+    b.addEventListener("click", Totali);
+})
+let option = new Map();
+option.set("water1", 120);
+option.set("water2", 60);
+option.set("water3", 45);
+option.set("water4", 38);
+option.set("water5", 42);
+function Totali() {
+    event.preventDefault();
+    let cnt = document.getElementById("reqest").value;
+    let m = cnt.match(/^[0-9]+$/);
+    if (m !== null && cnt > 0)
+    {
+        let answer = document.getElementById("block").value;
+        let rez = "total cost of water: " + option.get(answer) * cnt;
+        document.getElementById("product").innerHTML = rez;
+    }
+    else 
+    {
+        alert("Wrong quantity");
+    }
+}
